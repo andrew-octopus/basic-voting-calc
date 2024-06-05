83a9159885e0f6728570dbc10049b6ca037a587e
Andrew Penland <andrewpenland@gmail.com>
2024-06-05 04:07:35 -0400

    Adjusted default weights, made modification to use ponits for comparability with other mechanism.

--- mechanisms/rank_n_slide_mechanism.py
+++ mechanisms/rank_n_slide_mechanism.py
@@ -4,30 +4,60 @@ Implements a weighted calculation method, where
 each voter has a score (based on their achievments) and assigns a proportion of that score to candidates.
 """
 
-from typing import Any, Dict, Callable
+from typing import Any, Callable, Dict, Literal
 from mechanisms.voting_mechanism import VotingMechanism
 
-#Set default amounts for each NFT to contribute to individual 
-DEFAULT_NFT_SCORES = {
-    "FUND_MOD1": 3.0,
-    "FUND_MOD2": 3.0,
-    "FUND_MOD3": 3.0,
-    "FUND_MOD4": 3.0,
-    "FUND_MOD5": 3.0,
-    "BARCAMP_PARIS_23": 2.0,
-    "NFTREP_V1": 1.0,
-    "STUDY_GROUP_HOST_C2_22_23": 1.0,
-    "STUDY_GROUP_HOST_360_22": 1.0,
-    "STUDY_GROUP_HOST_FUND_22_23": 1.0,
-    "LIVE_TRACK_1": 1.0,
-    "LIVE_TRACK_2": 1.0,
-    "LIVE_TRACK_3": 1.0,
-    "LIVE_TRACK_4": 1.0,
-    "LIVE_TRACK_5": 3.0,
-    "LIVE_TRACK_6": 3.0,
-    "LIVE_TRACK_7": 3.0,
-    "LIVE_TRACK_8": 3.0
-}
+# Deprecated: original default values
+# DEFAULT_NFT_SCORES = {
+#     "FUND_MOD1": 3.0,
+#     "FUND_MOD2": 3.0,
+#     "FUND_MOD3": 3.0,
+#     "FUND_MOD4": 3.0,
+#     "FUND_MOD5": 3.0,
+#     "BARCAMP_PARIS_23": 2.0,
+#     "NFTREP_V1": 1.0,
+#     "STUDY_GROUP_HOST_C2_22_23": 1.0,
+#     "STUDY_GROUP_HOST_360_22": 1.0,
+#     "STUDY_GROUP_HOST_FUND_22_23": 1.0,
+#     "LIVE_TRACK_1": 1.0,
+#     "LIVE_TRACK_2": 1.0,
+#     "LIVE_TRACK_3": 1.0,
+#     "LIVE_TRACK_4": 1.0,
+#     "LIVE_TRACK_5": 3.0,
+#     "LIVE_TRACK_6": 3.0,
+#     "LIVE_TRACK_7": 3.0,
+#     "LIVE_TRACK_8": 3.0
+# }
+
+# Set default NFT values suggested by TEA 
+DEFAULT_NFT_WEIGHTS = {'FUND_AUTHOR': 20.0,
+ 'SPEAKER_ETHCC_PARIS23': 16.0,
+ 'FUND_MOD_1': 7.0,
+ 'FUND_MOD_2': 7.0,
+ 'FUND_MOD_3': 7.0,
+ 'FUND_MOD_4': 7.0,
+ 'FUND_MOD_5': 7.0,
+ 'NFTREP_V1': 3.0,
+ 'ETHCC_23': 1.0,
+ 'SPEAKER_BARCAMP_PARIS_23': 12.0,
+ 'BARCAMP_PARIS_23': 5.0,
+ 'TEAM_BARCAMP_PARIS_23': 1.0,
+ 'STUDY_GROUP_HOST_FUND_22_23': 10.0,
+ 'STUDY_GROUP_HOST_C2_22_23': 10.0,
+ 'STUDY_GROUP_HOST_360_22': 10.0,
+ 'STUDY_SEASON_REGISTRATION': 1.0,
+ 'LIVE_TRACK_1': 10.0,
+ 'LIVE_TRACK_3': 10.0,
+ 'LIVE_TRACK_4': 10.0,
+ 'LIVE_TRACK_5': 10.0,
+ 'LIVE_TRACK_6': 10.0,
+ 'LIVE_TRACK_7': 10.0,
+ 'LIVE_TRACK_8': 10.0,
+ 'FELLOWSHIP_COMM': 16.0,
+ 'STUDY_SEASON_SPEAKER': 16.0,
+ 'FUND_WE_MADE_IT': 10.0,
+ 'FUND_MOD_3_AND_4': 5.0,
+ 'FUND_ALL': 10.0}
 
 class RankAndSlide(VotingMechanism):
     """
@@ -45,11 +75,11 @@ class RankAndSlide(VotingMechanism):
     """
 
     def __init__(self, 
-                 ):
+                 credential_info: Dict[str, float] = DEFAULT_NFT_WEIGHTS):
         super().__init__()
-        self.weighing_mechanism = self.get_default_weighing_mechanism
+        # self.weighing_mechanism = self.get_default_weighing_mechanism(credential_info_to_use = DEFAULT_NFT_WEIGHTS)
 
-    def calculate(self, voters: Dict[str, Dict[str, int]], 
+    def calculate(self, voters: Dict[str, Dict[str, int]],
                   voter_choices: Dict[str, Dict[str, float]]):
         """
         Implements a score voting system.
@@ -71,15 +101,46 @@ class RankAndSlide(VotingMechanism):
 
             for candidate, proportion in normalised_choice.items():
                 if candidate in candidate_scores:
-                    candidate_scores[candidate] += proportion * self.calculate_score(voters.get(voter_id))
+                    candidate_scores[candidate] += proportion * voters.get(voter_id).get("points",0)
                 else:
-                    candidate_scores[candidate] = proportion * self.calculate_score(voters.get(voter_id))
-
+                    candidate_scores[candidate] = proportion *  voters.get(voter_id).get("points",0)
+        
         # Sort candidates by their score, and return the winner (along with the detailed scores)
         sorted_candidates = dict(sorted(candidate_scores.items(), key=lambda x: x[1], reverse=True))
         winner = max(candidate_scores, key=candidate_scores.get)
 
         return winner, candidate_scores
+    
+    def allocate_points_from_credentials(self,
+                                        voter_credentials: Dict[str, Dict[str,
+                                                Literal[0,1,None]]],
+                        credential_weights: Dict[str, float]):
+        """
+        Takes a dictionary of voter credentials, with weights for each credential, 
+        and applies the points available. 
+
+        In the TEA Reputation-Weighted example, the credentials are NFTs.
+        We set total_amount_to_allocate to 10_000 to avoid issues with small errors. 
+
+        NOTE: All voter_credentials should have values. 
+        """ 
+        # Create a blank dictionary to keep information
+        raw_voter_amounts = {}
+        total_amount_all_voters = 0 
+
+        # For each voter and their list of credentials
+        for voter, individual_voter_credentials in voter_credentials.items():
+            # Create an entry in the dictionary for their total weight
+            raw_voter_amounts[voter] = {"points": 0}
+            # Loop over their credentials and add the total amount 
+            for credential in individual_voter_credentials.keys():
+                    voter_has_credential = voter_credentials.get(voter).get(credential,0)
+                    individual_credential_weight = credential_weights.get(credential, 0) 
+                    weight_to_add_to_voter = voter_has_credential * individual_credential_weight
+                    raw_voter_amounts[voter]["points"] += weight_to_add_to_voter
+
+        return raw_voter_amounts
+
 
     def normalize_proportions(self, choice: Dict[str, float]):
         # So that if a vote is passed in that doesn't add up to 1 the numbers get adjusted.
@@ -88,16 +149,16 @@ class RankAndSlide(VotingMechanism):
         for candidate, proportion in choice.items():
             choice[candidate] = proportion / total
         return choice
-
+    
     def get_default_weighing_mechanism(self, 
                                        voter: Dict[str, int],
-                                       credential_info: Dict = DEFAULT_NFT_SCORES):
+                                       credential_info_to_use = None):
         score = 0
         for nft, amount in voter.items():
             if amount > 0:
                 # Take the score of the default NFT list, or 1 if it's not defined.
                 # If a user holds multiple (attended a session multiple times?), then add them up
-                score += amount * credential_info.get(nft, 0)
+                score += amount * credential_info_to_use.get(nft, 0)
         return score
         
 
@@ -107,4 +168,4 @@ class RankAndSlide(VotingMechanism):
 
     def calculate_score(self, voter):
         return self.weighing_mechanism(voter)
-
+    
