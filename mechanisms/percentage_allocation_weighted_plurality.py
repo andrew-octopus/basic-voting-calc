7d550b0277bd9ad9d4b11b4af52878420626b4f8
Andrew Penland <andrewpenland@gmail.com>
2024-06-18 00:03:26 -0400

    Removed safety checks in the interest of time.

--- mechanisms/percentage_allocation_weighted_plurality.py
+++ mechanisms/percentage_allocation_weighted_plurality.py
@@ -34,7 +34,7 @@ class PercentageAllocationWeightedPlurality(VotingMechanism):
 
         # Calculate the weighted vote for each candidate based on voter choices and voter weights
         for voter_id, ballot in voter_choices.items():
-            ballot_total = sum(ballot.values())
+            # "ballot_total = sum(ballot.values())
             # if not isclose(ballot_total, 1.0, rel_tol = 1e-5):
             #     raise ValueError("The voter's ballot should sum to 1.0")
             for choice, percentage in ballot.items():
